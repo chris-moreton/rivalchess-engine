58bf7c3b0be5f75e000bde59f4dff11f9e69996f
Chris Moreton <chris.moreton@hmcts.net>
2020-01-16 16:49:19 +0000

    Test failing EPD tests

--- src/test/java/com/netsensia/rivalchess/epd/EpdTest.java
+++ src/test/java/com/netsensia/rivalchess/epd/EpdTest.java
@@ -9,6 +9,7 @@ import com.netsensia.rivalchess.util.ChessBoardConversion;
 import com.netsensia.rivalchess.util.EpdItem;
 import com.netsensia.rivalchess.util.EpdReader;
 import com.netsensia.rivalchess.util.FenUtils;
+import org.awaitility.core.ConditionTimeoutException;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,9 +22,9 @@ import java.util.List;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class EpdTest {
@@ -37,17 +38,19 @@ public class EpdTest {
         new Thread(rivalSearch).start();
     }
 
-    private final List<String> excludeTests = Collections.unmodifiableList(Arrays.asList(
-            "WAC.002","WAC.008","WAC.020","WAC.061","WAC.167","WAC.168","WAC.201",
-            "WAC.209","WAC.213","WAC.229","WAC.230","WAC.251","WAC.259","WAC.268",
-            "WAC.269","WAC.274","WAC.276","WAC.281","WAC.296"
+    private final List<String> failingPositions = Collections.unmodifiableList(Arrays.asList(
+            "WAC.002","WAC.008","WAC.168","WAC.201","WAC.209",
+            "WAC.213","WAC.229","WAC.230","WAC.251","WAC.259",
+            "WAC.268","WAC.269","WAC.274","WAC.276","WAC.281",
+            "WAC.296"
     ));
 
-    private void testPosition(EpdItem epdItem) throws IllegalFenException, InterruptedException {
+    private void testPosition(EpdItem epdItem, boolean expectedToPass) throws IllegalFenException, InterruptedException {
 
         EngineChessBoard engineChessBoard = new EngineChessBoard();
         engineChessBoard.setBoard(FenUtils.getBoardModel(epdItem.getFen()));
 
+        rivalSearch.quit();
         rivalSearch.setBoard(engineChessBoard);
         rivalSearch.setSearchDepth(10);
         rivalSearch.setMillisToThink(RivalConstants.MAX_SEARCH_MILLIS);
@@ -58,13 +61,25 @@ public class EpdTest {
 
         System.out.println(epdItem.getId());
 
-        await().atMost(MAX_SEARCH_SECONDS, SECONDS).until(() -> !rivalSearch.isSearching());
+        try {
+            await().atMost(MAX_SEARCH_SECONDS, SECONDS).until(() -> !rivalSearch.isSearching());
+        } catch (ConditionTimeoutException e) {
+            assertFalse(expectedToPass);
+            return;
+        }
 
-        Assert.assertThat(epdItem.getBestMoves(),
-                hasItem(ChessBoardConversion.getPGNMoveFromCompactMove(rivalSearch.getCurrentMove(), engineChessBoard)));
+        final String move = ChessBoardConversion.getPGNMoveFromCompactMove(rivalSearch.getCurrentMove(), engineChessBoard);
+        System.out.println("Looking for " + move + " in " + epdItem.getBestMoves());
+
+        if (expectedToPass) {
+            Assert.assertThat(epdItem.getBestMoves(), hasItem(move));
+        } else {
+            Assert.assertTrue(!epdItem.getBestMoves().contains(ChessBoardConversion.getPGNMoveFromCompactMove(
+                    rivalSearch.getCurrentMove(), engineChessBoard)));
+        }
     }
 
-    public void runEpdSuite(String filename, String startAtId)
+    public void runEpdSuite(String filename, String startAtId, boolean expectedToPass)
             throws IOException, IllegalEpdItemException, IllegalFenException, InterruptedException {
 
         ClassLoader classLoader = getClass().getClassLoader();
@@ -72,20 +87,27 @@ public class EpdTest {
 
         EpdReader epdReader = new EpdReader(file.getAbsolutePath());
 
+        runEpdSuite(startAtId, epdReader, expectedToPass);
+    }
+
+    private void runEpdSuite(String startAtId, EpdReader epdReader, boolean expectedToPass) throws IllegalFenException, InterruptedException {
         boolean processTests = false;
 
         for (EpdItem epdItem : epdReader) {
             processTests = processTests || (epdItem.getId().equals(startAtId));
-            if (processTests) {
-                if (!excludeTests.contains(epdItem.getId())) {
-                    testPosition(epdItem);
-                }
+            if (processTests && expectedToPass != failingPositions.contains(epdItem.getId())) {
+                testPosition(epdItem, expectedToPass);
             }
         }
     }
 
     @Test
     public void winAtChess() throws IOException, IllegalEpdItemException, IllegalFenException, InterruptedException {
-        runEpdSuite("winAtChess.epd", "WAC.297");
+        runEpdSuite("winAtChess.epd", "WAC.001", true);
+    }
+
+    @Test
+    public void winAtChessFails() throws IOException, IllegalEpdItemException, IllegalFenException, InterruptedException {
+        runEpdSuite("winAtChess.epd", "WAC.001", false);
     }
 }
