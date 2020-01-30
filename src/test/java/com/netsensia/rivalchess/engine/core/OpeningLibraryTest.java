11e589ce8e39dc53179a01d39146607847e17212
Chris Moreton <chris.moreton@hmcts.net>
2020-01-30 11:59:22 +0000

    Generating constants in OpeningLibrary

--- src/test/java/com/netsensia/rivalchess/engine/core/OpeningLibraryTest.java
+++ src/test/java/com/netsensia/rivalchess/engine/core/OpeningLibraryTest.java
@@ -5,6 +5,7 @@ import com.netsensia.rivalchess.util.ChessBoardConversion;
 import com.netsensia.rivalchess.util.FenUtils;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,12 +33,38 @@ public class OpeningLibraryTest {
                         fens.add(newFen);
                         if (OpeningLibrary.getMove(newFen) != 0) {
                             System.out.println("public static final String " + newPath + " = \"" + newFen + "\";");
+
+                            // remove en passant part
+                            newFen = newFen.replaceAll(" a3", " -")
+                            .replaceAll(" b3", " -")
+                            .replaceAll(" c3", " -")
+                            .replaceAll(" d3", " -")
+                            .replaceAll(" e3", " -")
+                            .replaceAll(" f3", " -")
+                            .replaceAll(" g3", " -")
+                            .replaceAll(" h3", " -")
+                            .replaceAll(" a6", " -")
+                            .replaceAll(" b6", " -")
+                            .replaceAll(" c6", " -")
+                            .replaceAll(" d6", " -")
+                            .replaceAll(" e6", " -")
+                            .replaceAll(" f6", " -")
+                            .replaceAll(" g6", " -")
+                            .replaceAll(" h6", " -");
+
+                            ProcessBuilder processBuilder = new ProcessBuilder();
+
+                            String regex = "'s/\"" + newFen.replaceAll("/","\\\\/") + "\"/" + newPath + "/g'";
+                            String command = "sed -i " + regex + " /home/chrismoreton/git/chris-moreton/rival-chess-android-engine/src/main/java/com/netsensia/rivalchess/engine/core/OpeningLibrary.java";
+                            processBuilder.command("bash", "-c", command);
+                            processBuilder.start();
+                            Thread.sleep(1000);
                             makeMove(newFen, newPath);
                         }
                     }
                     engineBoard.unMakeMove();
                 }
-            } catch (ArrayIndexOutOfBoundsException e) {
+            } catch (ArrayIndexOutOfBoundsException | IOException | InterruptedException e) {
                 System.out.println(sMove + " from  " + fen + " is dodgy!");
                 System.exit(0);
             }
