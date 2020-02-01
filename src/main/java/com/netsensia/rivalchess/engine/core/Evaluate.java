62d08f1f499942d6e4469c20c5f174a885596f18
Chris Moreton <chris.moreton@hmcts.net>
2020-02-01 21:26:52 +0000

    Refactoring RightWay evaluation

--- src/main/java/com/netsensia/rivalchess/engine/core/Evaluate.java
+++ src/main/java/com/netsensia/rivalchess/engine/core/Evaluate.java
@@ -6,88 +6,121 @@ public class Evaluate {
 
     private static final int KINGSAFETY_RIGHTWAY_DIVISOR = 4;
 
-    private static int scoreRightWayPositions(EngineChessBoard board, int h1, int h2, int h3, int g2, int g3, int f1, int f2, int f3, int f4, int offset, int cornerColour) {
+    private static int scoreRightWayPositions(EngineChessBoard board, int h1, int h2, int h3, int g2, int g3, int f1, int f2, int f3, int f4, boolean isWhite, int cornerColour) {
         int safety = 0;
+        final int offset = isWhite ? 0 : 6;
 
-        if ((board.pieceBitboards[RivalConstants.ALL] & (1L << h1)) != 0) {
+        if (((board.pieceBitboards[RivalConstants.ALL] & (1L << h1)) != 0) ||
+                ((board.pieceBitboards[RivalConstants.WR + offset] & (1L << f1)) == 0)) {
             return 0;
         }
 
-        if ((board.pieceBitboards[RivalConstants.WR + offset] & (1L << f1)) != 0) {
-            if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << f2)) != 0) {
-                if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << g2)) != 0) {
-                    if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0) {
-                        // (A)
-                        safety += 120;
-                    } else {
-                        if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h3)) != 0) {
-                            if ((board.pieceBitboards[RivalConstants.WN + offset] & (1L << f3)) != 0) {
-                                // (D)
-                                safety += 70;
-                                // check for bishop of same colour as h3
-                                long bits = (cornerColour == RivalConstants.WHITE ? Bitboards.LIGHT_SQUARES : Bitboards.DARK_SQUARES);
-                                if ((bits & board.pieceBitboards[RivalConstants.WB + offset]) != 0) {
-                                    safety -= 30;
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << g3)) != 0) {
-                        if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0) {
-                            if ((board.pieceBitboards[RivalConstants.WB + offset] & (1L << g2)) != 0) {
-                                // (B)
-                                safety += 100;
-                            }
-                        } else {
-                            if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h3)) != 0) {
-                                if ((board.pieceBitboards[RivalConstants.WB + offset] & (1L << g2)) != 0) {
-                                    // (C)
-                                    safety += 70;
-                                }
-                            }
-                        }
-                    }
-                }
+        if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << f2)) != 0) {
+            if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << g2)) != 0) {
+                safety = checkForPositions_A_or_D(board, h2, h3, f3, isWhite, cornerColour, safety);
             } else {
-                if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << f4)) != 0) {
-                    if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << g2)) != 0) {
-                        if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0) {
-                            // (E)
-                            safety += 50;
-                            if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0) {
-                                if ((board.pieceBitboards[RivalConstants.WN + offset] & (1L << f3)) != 0) {
-                                    safety += 40;
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << f3)) != 0) {
-                        if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << g2)) != 0) {
-                            if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0) {
-                                // (F)
-                                safety += 25;
-                            } else {
-                                if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h3)) != 0) {
-                                    // (H)
-                                    safety -= 30;
-                                }
-                            }
-                        }
-                    }
-                }
+                safety = checkForPositionsB_or_C(board, h2, h3, g2, g3, isWhite, safety);
+            }
+        } else {
+            if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << f4)) != 0) {
+                safety = checkForPositionE(board, h2, g2, f3, isWhite, safety);
+            } else {
+                safety = checkForPositionFOrH(board, h2, h3, g2, f3, isWhite, safety);
             }
         }
 
         return safety / KINGSAFETY_RIGHTWAY_DIVISOR;
     }
 
+    private static int checkForPositionFOrH(EngineChessBoard board, int h2, int h3, int g2, int f3, boolean isWhite, int safety) {
+        final int offset = isWhite ? 0 : 6;
+
+        if (((board.pieceBitboards[RivalConstants.WP + offset] & (1L << f3)) != 0)
+            && ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << g2)) != 0)) {
+                if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0) {
+                    // (F)
+                    safety += 25;
+                } else {
+                    if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h3)) != 0) {
+                        // (H)
+                        safety -= 30;
+                    }
+                }
+        }
+        return safety;
+    }
+
+    private static int checkForPositionE(EngineChessBoard board, int h2, int g2, int f3, boolean isWhite, int safety) {
+        final int offset = isWhite ? 0 : 6;
+
+        if (((board.pieceBitboards[RivalConstants.WP + offset] & (1L << g2)) != 0)
+            && ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0)) {
+                // (E)
+                safety += 50;
+                if (((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0)
+                    && ((board.pieceBitboards[RivalConstants.WN + offset] & (1L << f3)) != 0)) {
+                        safety += 40;
+                    }
+        }
+        return safety;
+    }
+
+    private static int checkForPositionsB_or_C(EngineChessBoard board, int h2, int h3, int g2, int g3, boolean isWhite, int safety) {
+        final int offset = isWhite ? 0 : 6;
+
+        if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << g3)) != 0) {
+            if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0) {
+                if ((board.pieceBitboards[RivalConstants.WB + offset] & (1L << g2)) != 0) {
+                    // (B)
+                    safety += 100;
+                }
+            } else {
+                if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h3)) != 0) {
+                    if ((board.pieceBitboards[RivalConstants.WB + offset] & (1L << g2)) != 0) {
+                        // (C)
+                        safety += 70;
+                    }
+                }
+            }
+        }
+        return safety;
+    }
+
+    private static int checkForPositions_A_or_D(EngineChessBoard board, int h2, int h3, int f3, boolean isWhite, int cornerColour, int safety) {
+        final int offset = isWhite ? 0 : 6;
+
+        if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h2)) != 0) {
+            // (A)
+            safety += 120;
+        } else {
+            safety = checkForPosition_D(board, h3, f3, isWhite, cornerColour, safety);
+        }
+
+        return safety;
+    }
+
+    private static int checkForPosition_D(EngineChessBoard board, int h3, int f3, boolean isWhite, int cornerColour, int safety) {
+        final int offset = isWhite ? 0 : 6;
+
+        if ((board.pieceBitboards[RivalConstants.WP + offset] & (1L << h3)) != 0) {
+            if ((board.pieceBitboards[RivalConstants.WN + offset] & (1L << f3)) != 0) {
+                // (D)
+                safety += 70;
+                // check for bishop of same colour as h3
+                long bits = (cornerColour == RivalConstants.WHITE ? Bitboards.LIGHT_SQUARES : Bitboards.DARK_SQUARES);
+                if ((bits & board.pieceBitboards[RivalConstants.WB + offset]) != 0) {
+                    safety -= 30;
+                }
+            }
+        }
+        return safety;
+    }
+
     public static int getWhiteKingRightWayScore(EngineChessBoard engineChessBoard) {
 
         if (engineChessBoard.m_whiteKingSquare == 1 || engineChessBoard.m_whiteKingSquare == 8) {
             return scoreRightWayPositions(engineChessBoard,
-                    0, 8, 16, 9, 17, 2, 10, 18, 26, 0,
+                    0, 8, 16, 9, 17, 2, 10, 18, 26, true,
                     RivalConstants.WHITE);
         }
 
@@ -97,7 +130,7 @@ public class Evaluate {
     public static int getBlackKingRightWayScore(EngineChessBoard engineChessBoard) {
         if (engineChessBoard.m_blackKingSquare == 57 || engineChessBoard.m_blackKingSquare == 48) {
             return scoreRightWayPositions(engineChessBoard,
-                    56, 48, 40, 49, 41, 58, 50, 42, 34,6,
+                    56, 48, 40, 49, 41, 58, 50, 42, 34,false,
                     RivalConstants.BLACK);
         }
 
