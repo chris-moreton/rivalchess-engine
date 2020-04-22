50401ae2ba833fb8f53cf61c17a39a0291c1be27
Chris Moreton <chris.moreton@hmcts.net>
2020-04-22 16:14:53 +0100

    WIP

--- src/main/java/com/netsensia/rivalchess/engine/core/hash/SearchHashHelper.java
+++ src/main/java/com/netsensia/rivalchess/engine/core/hash/SearchHashHelper.java
@@ -8,7 +8,6 @@ import com.netsensia.rivalchess.engine.core.EngineChessBoard;
 import com.netsensia.rivalchess.enums.HashIndex;
 import com.netsensia.rivalchess.enums.HashValueType;
 import com.netsensia.rivalchess.exception.HashVerificationException;
-import com.netsensia.rivalchess.engine.core.RivalConstants;
 import com.netsensia.rivalchess.model.SquareOccupant;
 
 public class SearchHashHelper {
@@ -46,12 +45,12 @@ public class SearchHashHelper {
         final long hashValue = boardHash.getTrackedHashValue();
         final int hashIndex = boardHash.getHashIndex(board);
 
-        if (boardHash.getHashTableIgnoreHeight(hashIndex + RivalConstants.HASHENTRY_HEIGHT) >= depthRemaining &&
-                boardHash.getHashTableIgnoreHeight(hashIndex + RivalConstants.HASHENTRY_FLAG) != RivalConstants.EMPTY &&
-                boardHash.getHashTableIgnoreHeight(hashIndex + RivalConstants.HASHENTRY_64BIT1) == (int) (hashValue >>> 32) &&
-                boardHash.getHashTableIgnoreHeight(hashIndex + RivalConstants.HASHENTRY_64BIT2) == (int) (hashValue & Bitboards.LOW32)) {
+        if (boardHash.getHashTableIgnoreHeight(hashIndex + HashIndex.HASHENTRY_HEIGHT.getIndex()) >= depthRemaining &&
+                boardHash.getHashTableIgnoreHeight(hashIndex + HashIndex.HASHENTRY_FLAG.getIndex()) != HashValueType.EMPTY.getIndex() &&
+                boardHash.getHashTableIgnoreHeight(hashIndex + HashIndex.HASHENTRY_64BIT1.getIndex()) == (int) (hashValue >>> 32) &&
+                boardHash.getHashTableIgnoreHeight(hashIndex + HashIndex.HASHENTRY_64BIT2.getIndex()) == (int) (hashValue & Bitboards.LOW32)) {
 
-            boolean isLocked = boardHash.getHashTableVersion() - boardHash.getHashTableUseHeight(hashIndex + RivalConstants.HASHENTRY_VERSION) <= SearchConfig.MAXIMUM_HASH_AGE.getValue();
+            boolean isLocked = boardHash.getHashTableVersion() - boardHash.getHashTableUseHeight(hashIndex + HashIndex.HASHENTRY_VERSION.getIndex()) <= SearchConfig.MAXIMUM_HASH_AGE.getValue();
 
             if (isLocked) {
                 superVerifyAlwaysReplaceHash(board);
