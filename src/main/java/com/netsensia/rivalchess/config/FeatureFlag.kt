3a45a1359afa9a2df6823b9efdfaca576b10c55a
Chris Moreton <chris.moreton@hmcts.net>
2020-05-27 12:48:50 +0100

    25

--- src/main/java/com/netsensia/rivalchess/config/FeatureFlag.kt
+++ src/main/java/com/netsensia/rivalchess/config/FeatureFlag.kt
@@ -7,8 +7,8 @@ enum class FeatureFlag(val isActive: Boolean) {
     USE_ASPIRATION_WINDOW(true), // +3 fails when turned off
     USE_NULL_MOVE_PRUNING(true), // +17 fails when turned off
     USE_DELTA_PRUNING(false), // (1) 20 - best so far - turned off
-    USE_FUTILITY_PRUNING(false), // +4 fails when turned off
-    USE_HISTORY_HEURISTIC(true),
+    USE_FUTILITY_PRUNING(true), // +4 fails when turned off
+    USE_HISTORY_HEURISTIC(false), // +5 fails when turned off
     USE_MATE_HISTORY_KILLERS(true),
     USE_INTERNAL_OPENING_BOOK(true),
     USE_PIECE_SQUARES_IN_MOVE_ORDERING(true),
