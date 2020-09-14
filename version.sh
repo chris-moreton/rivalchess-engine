1d2f6ac6938bf9ce8d16c866728a6bf505cdf7b1
Chris Moreton <chris@netsensia.com>
2020-09-14 13:19:44 +0100

    Add version.sh script

--- version.sh
+++ version.sh
@@ -21,5 +21,4 @@ sed -i "s/version = .*/version = '$1'/g" build.gradle
 sed -i "s/<version>.*<\/version>/<version>'$1'<\/version>/g" README.md
 sed -i "s/version: '.*'/version: '$1'/g" README.md
 ./gradlew publishToMavenLocal
-source ~/.bashrc
 gittag $1
