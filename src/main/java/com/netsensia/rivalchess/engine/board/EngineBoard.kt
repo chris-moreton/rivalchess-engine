0abdb84a10d4c232f21527736d038dd1a70ebb12
Chris Moreton <chris@netsensia.com>
2020-09-23 13:52:40 +0100

    Tagging as 31.0.66

--- src/main/java/com/netsensia/rivalchess/engine/board/EngineBoard.kt
+++ src/main/java/com/netsensia/rivalchess/engine/board/EngineBoard.kt
@@ -76,13 +76,15 @@ class EngineBoard @JvmOverloads constructor(board: Board = getBoardModel(FEN_STA
     }
 
     fun getWhiteKingSquareCalculated(): Int {
-        val retVal = getBitboard(BITBOARD_WK).countTrailingZeroBits()
+        val retVal = whiteKingSquareTracked
+        //val retVal = getBitboard(BITBOARD_WK).countTrailingZeroBits()
         //if (retVal != whiteKingSquareTracked) aText()
         return retVal
     }
 
     fun getBlackKingSquareCalculated(): Int {
-        val retVal = getBitboard(BITBOARD_BK).countTrailingZeroBits()
+        val retVal = blackKingSquareTracked
+        //val retVal = getBitboard(BITBOARD_BK).countTrailingZeroBits()
         //if (retVal != blackKingSquareTracked) aText()
         return retVal
     }
